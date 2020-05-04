7b655bb16915f68a7287dcb34c91618c509c22be
srimukh <srimukh.prasad@gmail.com>
2020-05-04 01:37:23 -0700

    sp

--- UniProt_data/active_site_datagen.py
+++ UniProt_data/active_site_datagen.py
@@ -19,12 +19,11 @@ from UniRep.unirep import babbler1900 as babbler
 
 parser = argparse.ArgumentParser(description='Making UniRep+aa_rep for amino acid squences')
 parser.add_argument('--filename', type=str, required=True)
-parser.add_argument('--save_dir', type=str, required=True)
 parser.add_argument('--start', type=int, required=True)
 parser.add_argument('--stop', type=int, required=True)
-parser.add_argument('--init_path', type=str, required=True)
 parser.add_argument('--rnn_size', type=int, required=True)
 parser.add_argument('--n_layers', type=int, required=True)
+parser.add_argument('--batch_size', type=int, required=True)
 
 args = parser.parse_args()
 print(args)
@@ -34,11 +33,14 @@ rnn_size = args.rnn_size
 n_layers = args.n_layers
 initialize_mLSTM_from_scratch = False 
 initialize_toplayer_from_scratch = True # does not matter for babbler unless doing Evotuning
-batch_size = 1
-model_path = '../UniRep/'+args.init_path
+batch_size = args.batch_size
+
+init_path = '{}_weights/'.format(rnn_size)
+model_path = '../UniRep/'+init_path
 len_unirep = rnn_size
 
 print('Loading weights from ', model_path)
+save_dir = 'hs{}_nl{}_{}_to_{}'.format(rnn_size, args.n_layers, args.start, args.stop)
 # Amino acid representations
 
 use_dmpnn = True
@@ -51,6 +53,7 @@ if use_dmpnn:
         lst = aa_reps[key][1:-1].split(',')
         lst = [float(x) for x in lst]
         lst = np.array(lst)
+        lst = lst.reshape((-1,1))
         aa_reps[key] = lst
     len_aa_rep = len(aa_reps['M'])
 else:
@@ -60,13 +63,20 @@ len_rep = len_unirep+len_aa_rep
 b_trained = babbler(batch_size=batch_size, model_path=model_path, trained=(not initialize_toplayer_from_scratch),
                    rnn_size=rnn_size, n_layers=n_layers, new=initialize_mLSTM_from_scratch)
 
-def make_xy(row):
+def make_xy(row, batch_size):
     seq = row['Sequence']
     act_site = row['Active site residue']
-    act_site_type = row['Active site type']
-    avg_hidden, final_hidden, final_cell = b_trained.get_rep(seq)
-    mol_rep = aa_reps[act_site]
-    return np.concatenate((np.reshape(final_hidden, (-1,)), mol_rep)), int(act_site_type-1)
+    act_site_type = np.array(row['Active site type'])
+    avg_hidden, final_hidden, final_cell = b_trained.get_reps(seq, batch_size)
+    mol_rep = np.zeros((len_aa_rep, batch_size))
+    count=0
+    for i in act_site.index.tolist():
+        mol_rep[:,count:count+1] = aa_reps[act_site[i]]
+        count+=1
+    final_hidden = final_hidden.T
+    act_site_type = act_site_type-1
+    act_site_type = act_site_type.reshape(act_site_type.shape[0],1)
+    return np.concatenate((final_hidden, mol_rep), axis=0), act_site_type.T
 
 start = args.start 
 stop = args.stop
@@ -74,27 +84,31 @@ stop = args.stop
 df_active_site = pd.read_excel(args.filename ,header=[0],index_col=[0])
 df_active_site = df_active_site[start:stop]
 
-m = stop - start
-X_train = np.zeros((len_rep, m))
-Y_train = np.zeros((m,1))
+num_batches = math.floor((stop - start)/batch_size)
+print('num_batches: ', num_batches)
 
-for row in range(m):
-    if (row % 10 ==0):
-        print('Writing row number :', row)
-        np_concat = np.concatenate((X_train, Y_train.T), axis=0)
+X_train = np.zeros((len_rep, num_batches*batch_size))
+Y_train = np.zeros((1, num_batches*batch_size))
 
-        print('Saving to ', args.save_dir)
-        np.save(args.save_dir, np_concat)
+for i in range(num_batches):
+    if (i % 10 ==0):
+        print('Writing batch number :', i)
+        print('X_train shape: ', X_train.shape)
+        print('Y_train shape: ', Y_train.shape)
+        np_concat = np.concatenate((X_train, Y_train), axis=0)
 
-    X_train[:,row], Y_train[row] = make_xy(df_active_site.iloc[row])
+        print('Saving to ', save_dir)
+        np.save(save_dir, np_concat)
+    
+    X_train[:,i*batch_size:(i+1)*batch_size], Y_train[:,i*batch_size:(i+1)*batch_size ] = make_xy(df_active_site.iloc[i*batch_size:(i+1)*batch_size], batch_size)
 
 
 print('X_train shape: ', X_train.shape)
 print('Y_train shape: ', Y_train.shape)
 
-np_concat = np.concatenate((X_train, Y_train.T), axis=0)
+np_concat = np.concatenate((X_train, Y_train), axis=0)
 
-print('Saving to ', args.save_dir)
-np.save(args.save_dir, np_concat)
+print('Saving to ', save_dir)
+np.save(save_dir, np_concat)
 
 print('Done saving to file successfully')
