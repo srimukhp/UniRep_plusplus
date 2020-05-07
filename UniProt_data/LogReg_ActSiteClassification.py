82e63c1b8b7ea9af4e051cfed3897e87212c66d1
srimukh <srimukh.prasad@gmail.com>
2020-05-07 01:28:28 -0700

    sp

--- UniProt_data/LogReg_ActSiteClassification.py
+++ UniProt_data/LogReg_ActSiteClassification.py
@@ -6,8 +6,6 @@ import sys
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from IPython.display import clear_output
 from six.moves import urllib
 import json
 import math
