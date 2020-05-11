04e9d6e76d959facd24220ba43e08b78d64ca64f
Forest Yang <forestyang@berkeley.edu>
2020-05-10 23:36:31 -0700

    sasdfdsfdsafdsfadsfasdf train scrippppt and loadddata

--- train_script.py
+++ train_script.py
@@ -6,6 +6,7 @@ import argparse
 import os
 from UniRep.unirep import babbler1900 as babbler
 import matplotlib.pyplot as plt
+import pickle
 
 def get_sites(string, site_type='ACT_SITE'):
     """string=site entry
@@ -23,9 +24,39 @@ def nonpad_len(batch):
     lengths = np.sum(nonzero, axis=1)/2
     return lengths
 
+def write_formatted(df, b, fname='formatted.txt'):
+    with open(fname, "w") as destination:
+        for i,row in df.iterrows():
+            seq = row['Sequence']
+            site_seq = np.array(row['Site sequence'][1:-1].replace('\n', '').split(' '), dtype=np.int32)
+            if b.is_valid_seq(seq): 
+                seq = b.format_seq(seq)
+                interleaved_seq = np.zeros(2*len(seq), dtype=np.int32)
+                # interleave the amino acid sequence and site sequence
+                # because this is how we are storing both datas
+                interleaved_seq[::2] = seq
+                interleaved_seq[1::2] = site_seq+1
+                formatted = ",".join(map(str,interleaved_seq))
+                destination.write(formatted)
+                destination.write('\n')
+
+def run_eval():
+    batch = sess.run(test_bucket_op)
+    seq_batch = batch[:, ::2]
+    site_seq_batch = batch[:, 1::2]-1
+    length = nonpad_len(batch)-1
+    loss, site_loss = sess.run([b._loss, b.site_loss], 
+                                feed_dict = {
+                                x_placeholder: seq_batch[:, :-1],
+                                b._minibatch_y_placeholder: seq_batch[:, 1:],
+                                b._minibatch_site_y_placeholder: site_seq_batch[:, :-1],
+                                initial_state_placeholder: b._zero_state,
+                                seq_length_placeholder: length,
+                                batch_size_placeholder: batch_size,
+                                b._temp_placeholder: 0.1})
+    return loss, site_loss
+
 get_binding_sites = lambda string: get_sites(string, 'BINDING')
-
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='train unirep')
@@ -35,13 +66,14 @@ if __name__ == '__main__':
     parser.add_argument('--n_layers', type=int, default=4)
     parser.add_argument('--save_dir', type=str)
     parser.add_argument('--site_loss', type=bool, default=False)
+    parser.add_argument('--trained', type=bool, default=True)
 
     args = parser.parse_args()
     print(args)
 
     if not os.path.exists('processed_uniprot.csv'):
         print("processing uniprot data and saving to csv for future reference")
-        df = pd.read_csv('UniProt_data/uniprot.tab', sep='\t', lineterminator='\n')
+        df = pd.read_csv('uniprot.tab', sep='\t', lineterminator='\n')
         df = df.fillna('')
         df.loc[:, 'Active site'] = df.loc[:, 'Active site'].apply(get_sites)
         df.loc[:, 'Binding site'] = df.loc[:, 'Binding site'].apply(get_binding_sites)
@@ -59,7 +91,7 @@ if __name__ == '__main__':
         df = pd.read_csv('processed_uniprot.csv')
 
     batch_size = 12
-    b = babbler(batch_size=batch_size, model_path=args.init_path, trained=args.init_path != '.', 
+    b = babbler(batch_size=batch_size, model_path=args.init_path, trained=(args.init_path != '.') and args.trained, 
                 rnn_size=args.rnn_size, n_layers=args.n_layers, new=args.init_path == '.')
 
     df_le = df[df['Length']<400]
@@ -68,36 +100,26 @@ if __name__ == '__main__':
     df_test = df_le[test_mask]
     print(f"Size of training data: {len(df_tr)}, size of test data: {len(df_test)}")
 
-    with open("formatted.txt", "w") as destination:
-        for i,row in df_tr.iterrows():
-            seq = row['Sequence']
-            site_seq = np.array(row['Site sequence'][1:-1].replace('\n', '').split(' '), dtype=np.int32)
-            if b.is_valid_seq(seq): 
-                seq = b.format_seq(seq)
-                interleaved_seq = np.zeros(2*len(seq), dtype=np.int32)
-                # interleave the amino acid sequence and site sequence
-                # because this is how we are storing both datas
-                interleaved_seq[::2] = seq
-                interleaved_seq[1::2] = site_seq+1
-                formatted = ",".join(map(str,interleaved_seq))
-                destination.write(formatted)
-                destination.write('\n')
+    write_formatted(df_tr, b, 'formatted.txt')
+    write_formatted(df_test, b, 'test_formatted.txt')
 
-    bucket_op = b.bucket_batch_pad("formatted.txt", lower=200, interval=1000) # Large interval
+    bucket_op = b.bucket_batch_pad("formatted.txt", lower=200, interval=200)
+    test_bucket_op = b.bucket_batch_pad("test_formatted.txt", lower=200, interval=200)
     final_hidden, x_placeholder, batch_size_placeholder, seq_length_placeholder, initial_state_placeholder = (
     b.get_rep_ops())
     optimizer = tf.train.AdamOptimizer(0.001)
+
     if args.site_loss: 
         all_step_op = optimizer.minimize(b._loss+b.site_loss)
     else:
         print("No site loss!")
         all_step_op = optimizer.minimize(b._loss)
 
-
     if not os.path.exists(args.save_dir):
         os.mkdir(args.save_dir)
 
     losses, site_losses = [], [] 
+    test_losses, test_site_losses = [], [] 
     print(f"START OF TRAINING FOR {args.num_iters} ITERATIONS")
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
@@ -105,9 +127,6 @@ if __name__ == '__main__':
             batch = sess.run(bucket_op)
             seq_batch = batch[:, ::2]
             site_seq_batch = batch[:, 1::2]-1
-            if np.max(site_seq_batch) >= 3:
-                print("WHOA.... SOMETHING WENT WRONG WITH THIS AMINO ACID:")
-                print(seq_batch[np.argmax(np.max(site_seq_batch, axis=1))])
             length = nonpad_len(batch)-1
             if args.site_loss:
                 loss, site_loss, _= sess.run([b._loss, b.site_loss, all_step_op],
@@ -135,6 +154,15 @@ if __name__ == '__main__':
                                                })
                 losses.append(loss)
                 print(f'Iteration {i}, loss {loss}')
+            if i % 10 == 0:
+                sum_test_loss, sum_test_site_loss = 0,0
+                for ti in range(3):
+                    test_loss, test_site_loss = run_eval()
+                    sum_test_loss += test_loss
+                    sum_test_site_loss += test_site_loss
+                test_losses.append(sum_test_loss/3)
+                test_site_losses.append(sum_test_site_loss/3)
+                print(f"Iteration {i}, test loss {test_losses[-1]}, site loss {test_site_losses[-1]}")
         b.dump_weights(sess, args.save_dir)
 
     fig, ax = plt.subplots(1,2, figsize=(13, 5))
@@ -150,3 +178,24 @@ if __name__ == '__main__':
         ax[1].set_title('Site type (normal/active/binding) loss')
 
     fig.savefig(f'{args.save_dir}/plot.png')
+
+    fig, ax = plt.subplots(1,2, figsize=(13, 5))
+    ax[0].plot(np.arange(len(test_losses)), test_losses)
+    ax[0].set_xlabel('iteration number')
+    ax[0].set_ylabel('Test loss')
+    ax[0].set_title('Next amino acid prediction loss (Test)')
+
+    ax[1].plot(np.arange(len(test_site_losses)), test_site_losses)
+    ax[1].set_xlabel('iteration number')
+    ax[1].set_ylabel('test site loss')
+    ax[1].set_title('Site type (normal/active/binding) loss (Test')
+
+    fig.savefig(f'{args.save_dir}/test_plot.png')
+
+    with open(f'{args.save_dir}/losses.pkl', 'wb') as f:
+        all_losses = {'train_losses': losses, 'test_losses': test_losses, 
+                      'test_site_losses': test_site_losses}
+        if args.site_loss:
+            all_losses['train_site_losses': site_losses]
+        pickle.dump(all_losses, f)
+
